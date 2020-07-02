f90fb91d64be6126bb36a52d8aa2ec5b5fb0ad18
Davep <53262435+Dee002@users.noreply.github.com>
2020-07-02 17:07:39 +0500

    Add files via upload

---
+++ Moduels and packeges/one.py
@@ -0,0 +1,16 @@
+#one.py
+
+
+def func():
+    print("Func() in one.py")
+
+def function():
+    pass
+def function2():
+    pass
+
+
+if __name__ == '__main__':
+    #Run the script
+    function()
+    function2()
