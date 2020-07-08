04bb54a841669b3ca3c64bab868b61c4e2e814a8
Davep <53262435+Dee002@users.noreply.github.com>
2020-07-08 21:31:24 +0500

    Create quizz.py

---
+++ Moduels and packeges/quizz.py
@@ -0,0 +1,41 @@
+#Coding chllange exersise
+def append_sum(lst):
+    lst.append(lst[-1] + lst[-2])
+    lst.append(lst[-1] + lst[-2])
+    lst.append(lst[-1] + lst[-2])
+    return lst
+
+##print(append_sum([1, 1, 2]))
+##Challange num 2
+def larger_list(lst1, lst2):
+    if len(lst1) >= len(lst2):
+        return lst1
+    else:
+        return lst2
+
+print(larger_list([4, 10, 2, 5], [-10, 2, 5, 10]))
+
+
+##Challange num 3
+def more_than_n(lst, item, n):
+  if lst.count(item) > n:
+    return True
+  else:
+    return False
+
+print(more_than_n([2, 4, 6, 2, 3, 2, 1, 2], 2, 3))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
