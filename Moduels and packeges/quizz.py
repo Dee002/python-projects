d863f2bd6a87603b7615208c8d89fda4979dbc08
Davep <53262435+Dee002@users.noreply.github.com>
2020-07-10 08:21:32 +0500

    Update quizz.py

--- Moduels and packeges/quizz.py
+++ Moduels and packeges/quizz.py
@@ -33,8 +33,17 @@ def append_size(lst):
 print(append_size([23, 42, 108])
 
 
-
-
+##Challange num3
+##Write a function named combine_sort that has two parameters named lst1 and lst2.
+#The function should combine these two lists into one new list and sort the result. Return the new sorted list.
+def combine_sort(lst1, lst2):
+      return sorted(lst1 + lst2)
+      
+print(combine_sort([4, 10, 2, 5], [-10, 2, 5, 10]))
+      
+      
+      
+    
 
 
 
