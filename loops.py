eefc4536dcaf96ee4b83a61563f01137c7112826
Davep <53262435+Dee002@users.noreply.github.com>
2020-07-12 07:38:09 +0500

    Update loops.py

--- loops.py
+++ loops.py
@@ -8,3 +8,11 @@ for game in board_games:
    print(game)
 for sport in sport_games:
   print(sport)
+
+
+#Using Range in Loops
+promise = "I will not chew gum in class"
+for i in range(6):
+  print(promise)
+
+#Infinite Loops
