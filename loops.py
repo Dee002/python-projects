14b7e7722f43e6e00d111c4dc0645a8c9897fd74
Davep <53262435+Dee002@users.noreply.github.com>
2020-07-20 15:19:41 +0500

    Update loops.py

--- loops.py
+++ loops.py
@@ -15,4 +15,73 @@ promise = "I will not chew gum in class"
 for i in range(6):
   print(promise)
 
-#Infinite Loops
+#while loop
+all_students = ["Alex", "Briana", "Cheri", "Daniele", "Dora", "Minerva", "Alexa", "Obie", "Arius", "Loki"]
+students_in_poetry = []
+
+while len(students_in_poetry) < 6:
+  student = all_students.pop()
+  students_in_poetry.append(student)
+  
+print(students_in_poetry)
+
+
+
+
+#Nested Loops
+
+sales_data = [[12, 17, 22], [2, 10, 3], [5, 12, 13]]
+
+scoops_sold = 0
+
+for location in sales_data:
+  for element in location:
+    scoops_sold += element
+    print(scoops_sold)
+  print(location)
+
+
+
+#List Comprehensions
+heights = [161, 164, 156, 144, 158, 170, 163, 163, 157]
+
+can_ride_coaster = (height for height in heights if height > 161)
+
+print(can_ride_coaster)
+
+
+#More List Comprehensions
+celsius = [0, 10, 15, 32, -5, 27, 3]
+
+fahrenheit = [temp*(9/5) + 32 for temp in celsius]
+
+print(fahrenheit)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
